cde4f06ed596983120d018190a790589d40c86a8
Donald Truong <dotruong@ucsd.edu>
2018-01-14 13:36:51 -0800

    Initial Commit

---
+++ WorkoutLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkoutLog
+//
+//  Created by Donald Truong on 1/14/18.
+//  Copyright © 2018 Donald Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
