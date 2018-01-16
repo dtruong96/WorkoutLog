91d199f4a13c1b315bbaa44712f28b1cf84e59a5
Unknown <dotruong@ucsd.edu>
2018-01-16 08:09:12 -0800

    Changed and deleted functions in WorkoutTablieViewController.swift

--- WorkoutLog/WorkoutTableViewController.swift
+++ WorkoutLog/WorkoutTableViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class WorkoutTableViewController: UITableViewController {
+    
+    var completedWorkouts : [Workout] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return  completedWorkouts.count
     }
 
     
