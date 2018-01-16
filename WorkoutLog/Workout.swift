22548f738bd14c1bb63719d3195fa7140b211df6
Unknown <dotruong@ucsd.edu>
2018-01-15 16:24:09 -0800

    Created necessary file for app

---
+++ WorkoutLog/Workout.swift
@@ -0,0 +1,18 @@
+//
+//  Workout.swift
+//  WorkoutLog
+//
+//  Created by Donald Truong on 1/15/18.
+//  Copyright © 2018 Donald Truong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Workout {
+    var name = " "
+    var date = " "
+    var completed = false
+}
+
+
