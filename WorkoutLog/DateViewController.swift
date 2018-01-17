512e0d1f560e3d9156ec7bf0625f31beb554024b
Unknown <dotruong@ucsd.edu>
2018-01-16 16:12:48 -0800

    Add new veiwcontroller files to accommodate sotryboard message

---
+++ WorkoutLog/DateViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DateViewController.swift
+//  WorkoutLog
+//
+//  Created by Donald Truong on 1/16/18.
+//  Copyright © 2018 Donald Truong. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ WorkoutLog/ExerciseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExerciseViewController.swift
+//  WorkoutLog
+//
+//  Created by Donald Truong on 1/16/18.
+//  Copyright © 2018 Donald Truong. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
