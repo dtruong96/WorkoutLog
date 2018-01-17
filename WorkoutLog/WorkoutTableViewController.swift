996fd3a840a99e20ebf4acb1d7bf0fbc2e0c5297
Unknown <dotruong@ucsd.edu>
2018-01-16 16:32:48 -0800

    Added examples to be featured on the WorkoutLog app

--- WorkoutLog/WorkoutTableViewController.swift
+++ WorkoutLog/WorkoutTableViewController.swift
@@ -14,9 +14,46 @@ class WorkoutTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createWorkoutExamples()
 
     }
 
+    func createWorkoutExamples() -> [Workout]{
+        
+        let BenchPress = Workout()
+        BenchPress.name = "Bench Press"
+        BenchPress.weight = "150"
+        BenchPress.reps = "3"
+        BenchPress.date = "January 1, 2018"
+        BenchPress.bestRecord = true
+        
+        let Squats = Workout()
+        Squats.name = "Squat"
+        Squats.weight = "200"
+        Squats.reps = "3"
+        Squats.date = "January 1, 2018"
+        Squats.bestRecord = true
+        
+        return [BenchPress, Squats]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let workoutCell = completedWorkouts[indexPath.row]
+        
+        if workoutCell.bestRecord {
+            cell.textLabel?.text = "" + workoutCell.name
+        } else{
+            cell.textLabel?.text = workoutCell.name
+        }
+        
+        return cell
+    }
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return  completedWorkouts.count
