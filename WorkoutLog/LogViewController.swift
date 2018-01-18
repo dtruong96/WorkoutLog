8cc7d5e523477332381be744a0b4eec01967cea8
Unknown <dotruong@ucsd.edu>
2018-01-18 15:19:55 -0800

    Added new view controller file and connected to storyboard

---
+++ WorkoutLog/LogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogViewController.swift
+//  WorkoutLog
+//
+//  Created by Donald Truong on 1/18/18.
+//  Copyright © 2018 Donald Truong. All rights reserved.
+//
+
+import UIKit
+
+class LogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
